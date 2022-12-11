669debb558ab3784363af60616bc37c38b838b1d
Luke Kelley <lj_kelley@hotmail.com>
2022-12-12 09:02:09 +1100

    issues with incrementing boxes

--- javascript/script.js
+++ javascript/script.js
@@ -247,31 +247,99 @@ console.log(randWord);
 
 let userGuess = "a";
 
-if (randWord.includes(userGuess)) {
-  console.log("This word has an A");
-} else {
-  console.log("This word does not have an A");
-}
+//If user input matches the letter in an array and the position in the array then true - turns green
+// If value of input matches a letter in the array then true - eventually turn yellow
 
-const btns = document.querySelectorAll("button");
+//function that takes a letter and postion and outputs a colour
+
+//set values instead of storing them. similar to setText.. set.style.background-color.
+
+//avoid over storing things.
+
+const btns = document.querySelectorAll(".letter");
 console.log(btns);
 
 const boxes = document.querySelectorAll(".grid-item");
 
-//Selects all grid boxes, need to change to select only one grid-item box.
-// Unsure if this is thr right direction for the rest of the application
+const remove = document.querySelector("#remove");
+
+const enter = document.querySelector("#enter");
+
+console.log(boxes);
+
+let i = 0;
+
+// boxes[i].style.backgroundColor = "lightcoral";
+// boxes[i].style.backgroundColor = "greenyellow";
+
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    createElement("p", btn.value, boxes);
+    boxes[i].textContent = btn.value;
+    boxes[i].value = btn.value;
+
+    ++i;
   });
 });
 
-const createElement = (elementType, text, parent) => {
-  const newElement = document.createElement(elementType);
+remove.addEventListener("click", () => {
+  --i;
+  boxes[i].textContent = " ";
+  boxes[i].value = " ";
+});
 
-  const textNode = document.createTextNode(text);
+let j = 0;
 
-  newElement.appendChild(textNode);
+// i = grid-box position
+// J increases by 5 everytime I click the enter button
+// to check the next 5 boxes
+// once I check the first 5 elements
+// I need to check the next 5 elements
+// therefore I need to increase i by 5 to check from i (5) to j (10). and so on.
+// This for loop resets after it hits the desired condition of i (0 incremented by 1) is less than j (5)
+// When i click enter the second time i starts from 0 and increments until it hits the desired condition is than j (5 now incremented by 5 equalling 10)
+// This means the box I'm checking for a correct letter in is the first set of 5 not the second set of 5
+enter.addEventListener("click", () => {
+  j = j + 5;
+  for (let i = 0; i < j; ) {
+    checkLetter(i, i);
+    i++;
+    console.log("letters in array", i);
+    console.log("box letter", i);
+  }
+});
 
-  parent.appendChild(newElement);
+const checkLetter = (index, box) => {
+  if (randWord[index] === boxes[box].textContent) {
+    boxes[box].style.backgroundColor = "greenyellow";
+  } else if (randWord.includes(boxes[box].textContent)) {
+    boxes[box].style.backgroundColor = "gold";
+  } else {
+    boxes[box].style.backgroundColor = "lightcoral";
+  }
 };
+
+// const createElement = (elementType, text, parent) => {
+//   const newElement = document.createElement(elementType);
+
+//   const textNode = document.createTextNode(text);
+
+//   newElement.appendChild(textNode);
+
+//   parent.appendChild(newElement);
+// };
+
+const arr = ["h", "e", "l", "l", "o"];
+console.log(arr);
+
+let input = ["h", "e", "l", "l", "o"];
+console.log(input);
+
+arr.map(i);
+
+input.map(i);
+
+if (arr[i].every() == input[i].every()) {
+  console.log("correct");
+} else {
+  console.log("incorrect");
+}
